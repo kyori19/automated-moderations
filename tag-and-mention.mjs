846584e8fda04532f5c1d3046ca5a8347251a9f3
kyori19 <kyori@accelf.net>
2024-02-18 21:19:00 +0900

    add media with mention spam

--- tag-and-mention.mjs
+++ tag-and-mention.mjs
@@ -1,41 +1,16 @@
 #!/usr/bin/env node
 
-const suspend = (mastodonHost, accessToken) => (id) => fetch(
-    `https://${mastodonHost}/api/v1/admin/accounts/${id}/action`,
-    {
-            method: 'post',
-            headers: {
-                'Authorization': `Bearer ${accessToken}`,
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({
-                type: 'suspend',
-            }),
-        },
-    );
+import common from './common.mjs';
 
-const exec = async () => {
-    const mastodonHost = process.env.MASTODON_HOST;
-    if (!mastodonHost) {
-        console.log('MASTODON_HOST is not set.');
-        return;
-    }
-    console.log(`Mastodon: ${mastodonHost}`);
-
-    const accessToken = process.env.ACCESS_TOKEN;
-    if (!accessToken) {
-        console.log('ACCESS_TOKEN is not set.');
-        return;
-    }
-
+const accounts = async (mastodonHost, accessToken) => {
     const hashtag = process.argv[2];
     if (!hashtag) {
         console.log('Hashtag is not provided.');
-        return;
+        return [];
     }
 
     console.log(`Listing statuses for hashtag #${hashtag}...`);
-    const accounts = await fetch(
+    return fetch(
         `https://${mastodonHost}/api/v1/timelines/tag/${hashtag}?remote=true&limit=40`,
         {
             headers: {
@@ -46,21 +21,10 @@ const exec = async () => {
         .then((res) => res.json())
         .then((statuses) => statuses
             .filter(({ account, mentions }) => account.username.length === 10 && mentions.length > 1)
-            .map(({ account, content }) => {
+            .map(({ account }) => {
                 console.log(`Found @${account.acct}.`);
                 return account.id;
-            }))
-        .then((accounts) => new Set(accounts));
-
-    if (accounts.size === 0) {
-        console.log('No accounts will be suspended.');
-        return;
-    }
-
-    console.log(`Suspending ${accounts.size} accounts...`);
-    await Promise.all(Array.from(accounts).map(suspend(mastodonHost, accessToken)));
-    console.log('Done!');
+            }));
 };
 
-await exec();
-setInterval(exec, 10 * 1000);
+await common(accounts);
